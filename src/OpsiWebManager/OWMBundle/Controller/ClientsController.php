96c92d3db0964ff5c280a9ed93a472b3556e07c8
Darkounet <guevara.antoine@gmail.com>
2011-11-16 10:27:52 +0100

    Ajout d'un layout

--- src/OpsiWebManager/OWMBundle/Controller/ClientsController.php
+++ src/OpsiWebManager/OWMBundle/Controller/ClientsController.php
@@ -18,39 +18,13 @@ class ClientsController extends Controller
 		$yaml=shell_exec("sudo opsi-admin -d method getClients_listOfHashes");
 		$clients=yaml_parse($yaml);
 		return $this->render('OWMBundle:Clients:voir.html.twig',array('clients' => $clients)); 
-#				array(
-#					'hostId' => $clients[0]['hostId'],
-#					'description' => $clients[0]['description'],
-#					'created' => $clients[0]['created'],
-#					'inventoryNumber' => $clients[0]['inventoryNumber'],
-#					'notes' => $clients[0]['notes'],
-#					'hardwareAdidress' => $clients[0]['hardwareAddress'],
-#					'lastSeen' => $clients[0]['lastSeen'],
-#					'oneTimePassword' => $clients[0]['oneTimePassword'],
-#					'depotId' => $clients[0]['depotId'],
-#					'opsiHostKey' => $clients[0]['opsiHostKey'],
-#					'ipAddress' => $clients[0]['ipAddress']
-#				     ));
 	}
 	public function modifierAction()
 	{
 
 		$yaml=shell_exec("sudo opsi-admin -d method getNetworkConfig_hash");
 		$info=yaml_parse($yaml);
-		return $this->render('OWMBundle:Clients:modifier.html.twig', 
-				array(
-					'depotDrive' => $info['depotDrive'],
-					'nextBootServiceURL' => $info['nextBootServiceURL'],
-					'utilsUrl' => $info['utilsUrl'],
-					'configUrl' => $info['configUrl'],
-					'utilsDrive' => $info['utilsDrive'],
-					'opsiServer' => $info['opsiServer'],
-					'nextBootServerType' => $info['nextBootServerType'],
-					'depotUrl' => $info['depotUrl'],
-					'depotId' => $info['depotId'],
-					'configDrive' => $info['configDrive'],
-					'winDomain' => $info['winDomain']
-				     ));
+		return $this->render('OWMBundle:Clients:modifier.html.twig',array('clients' => $clients)); 
 
 	}
 	public function validmodifAction()
